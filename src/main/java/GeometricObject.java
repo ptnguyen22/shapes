c3e1d6b69490ce3a292e9b5a419a5e540ab92d59
ptnguyen22 <123274273+ptnguyen22@users.noreply.github.com>
2023-01-26 18:27:51 -0500

    Update GeometricObject.java

--- src/main/java/GeometricObject.java
+++ src/main/java/GeometricObject.java
@@ -1,20 +1,8 @@
-// File name: pnA3
-
-// Written by: Peter Nguyen
+//Peter Nguyen
  
-// Description: Interface for shapes
-//              
-//              
-// Challenges: None, it was straight forward without much to add at this point
-//             
-// Time Spent: 10 mins
+// Description: Interface for shapes             
+//None, it was straight forward without much to add at this point
 
-// Revision History:
-// Date:        By:      Action:
-// ---------------------------------------------------
-/* 02/03/20   Peter Nguyen  Created class and abstract method getName()                   
- *                                                          
-*/  
 public interface GeometricObject {
     
     //Abstract method for all subclasses
